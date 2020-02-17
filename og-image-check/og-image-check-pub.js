6ffea749c49062d61666e4fb71b03c7615d1943f
nilaydatta <nilay.datta@live.com>
2020-02-17 17:36:25 +0530

    adding window.onload

--- og-image-check/og-image-check-pub.js
+++ og-image-check/og-image-check-pub.js
@@ -6,56 +6,58 @@
 */
 
 function op_og_image_check_pub(callback) {
-    var el = document.querySelector("head meta[property='og:image']");
-    var ogImageURL = el.content;
-    var ogImage = new Image();
-    ogImage.src = ogImageURL;
-    if (el) {
-        ogImage.onload = function() {
-            var ogImageWidth = ogImage.width;
-            var ogImageHeight = ogImage.height;
-            if(ogImageWidth > 0 && ogImageWidth == 215 && ogImageHeight > 0 && ogImageHeight == 215) {
-                callback(buildPassedObject('n/a', getCheckName('op_og_image_check_pub')));
-                return;
-            } else if (ogImageWidth > 0 && ogImageWidth != 215 && ogImageHeight > 0 && ogImageHeight != 215) {
-                callback(
-                    buildFailedObject(
-                        ogImageURL,
-                        getCheckName('op_og_image_check_pub'),
-                        "OG Image is of dimensions " + ogImageWidth + "x" + ogImageHeight + "px"
-                    )
-                );
-                return;
-            } else {
-                callback(
-                    buildFailedObject(
-                        ogImageURL,
-                        getCheckName('op_og_image_check_pub'),
-                        "OG Image has zero width!"
-                    )
-                );
-                return;
-            }
-        };
-        ogImage.onerror = function() {
-            callback(
-                buildFailedObject(
-                    ogImageURL,
-                    getCheckName('op_og_image_check_pub'),
-                    "OG Image is not found!"
-                )
-            );
-            return;
-        };
-    } else {
-        callback(
-            buildFailedObject(
-                ogImageURL,
-                getCheckName('op_og_image_check_pub'),
-                "OG Image is not found!"
-            )
-        );
-        return;
-    }
+	window.onload = function() {
+		var el = document.querySelector("head meta[property='og:image']");
+		var ogImageURL = el.content;
+		var ogImage = new Image();
+		ogImage.src = ogImageURL;
+		if (el) {
+			ogImage.onload = function() {
+				var ogImageWidth = ogImage.width;
+				var ogImageHeight = ogImage.height;
+				if(ogImageWidth > 0 && ogImageWidth == 215 && ogImageHeight > 0 && ogImageHeight == 215) {
+					callback(buildPassedObject('n/a', getCheckName('op_og_image_check_pub')));
+					return;
+				} else if (ogImageWidth > 0 && ogImageWidth != 215 && ogImageHeight > 0 && ogImageHeight != 215) {
+					callback(
+						buildFailedObject(
+							ogImageURL,
+							getCheckName('op_og_image_check_pub'),
+							"OG Image is of dimensions " + ogImageWidth + "x" + ogImageHeight + "px"
+						)
+					);
+					return;
+				} else {
+					callback(
+						buildFailedObject(
+							ogImageURL,
+							getCheckName('op_og_image_check_pub'),
+							"OG Image has zero width!"
+						)
+					);
+					return;
+				}
+			};
+			ogImage.onerror = function() {
+				callback(
+					buildFailedObject(
+						ogImageURL,
+						getCheckName('op_og_image_check_pub'),
+						"OG Image is not found!"
+					)
+				);
+				return;
+			};
+		} else {
+			callback(
+				buildFailedObject(
+					ogImageURL,
+					getCheckName('op_og_image_check_pub'),
+					"OG Image is not found!"
+				)
+			);
+			return;
+		}
+	}
 }
 qualityChecks.registerCheck(op_og_image_check_pub);
